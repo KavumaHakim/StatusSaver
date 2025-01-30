fe86a76582847c5a2630dd20194af2649864f06b
Hakim <hakimkavuma99@gmail.com>
2025-01-31 02:29:19 +0300

    MEGA COMMIT: Added progress bars for terminal, Fixd the copying mechanism Added renaming of files based on date modified And soome other minor fixes in anticipation for making this OOP

--- StatusSaver.py
+++ StatusSaver.py
@@ -2,10 +2,12 @@
 from glob import glob
 import os
 from time import sleep
+import datetime
+from tqdm import tqdm
 
-pics = glob("/sdcard/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/*.jpg")
-videos = glob("/sdcard/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/*.mp4")
-ANDROID = glob("/sdcard/*")
+ANDROID = glob("/storage/emulated/0/*")
+PICS = "/storage/emulated/0/Statuses/Pics"
+VIDS = "/storage/emulated/0/Statuses/Videos"
 
 def copy_selection():
     os.system("clear")
@@ -22,42 +24,63 @@ if choice == '4':
 	print('\nThank you for using Status Saver.')
 
 while choice != '4':
-	if "/sdcard/Statuses" in ANDROID:
+	if "/storage/emulated/0/Statuses" in ANDROID:
 		print("Statuses folder Exists")
 	else:
 		print("Making Statuses Folder")
-		os.system("mkdir /sdcard/Statuses")
-		os.system("mkdir /sdcard/Statuses/Pics")
-		os.system("mkdir /sdcard/Statuses/Videos")
+		os.makedirs("/sdcard/Statuses")
+		os.makedirs("/sdcard/Statuses/Pics")
+		os.makedirs("/sdcard/Statuses/Videos")
 	sleep(3)
 
 	if choice.lower() == '1':
-	    print("Saving Pictures")
-    
-	    for i in pics:
-	        print(f'Copying: {i}\n')
-	        os.system(f"cp {i} /sdcard/Statuses/Pics")
-	        sleep(0.3)
+		pics = glob('/storage/emulated/0/Android/media/com.whatsapp/Whatsapp/Media/.Statuses/*.jpg')
+		print("Saving Pictures")
+		for i in tqdm(pics):
+			date_made = datetime.datetime.fromtimestamp(os.path.getmtime(i))
+			extension = os.path.splitext(i)[1]
+			new_name = f"{date_made.strftime("%Y-%m-%d %H:%M:%S")}{extension}"
+			new_file_path = os.path.join(PICS, new_name)
+			with open(i, 'rb') as f:
+				picture = f.read()
+			with open(new_file_path, "wb") as f:
+				f.write(picture)
+			sleep(0.3)
 
 	elif choice.lower() == '2':
-	    print("Saving Videos")
-	    sleep(2)
-	    
-	    for i in videos:
-	        print(f'Copying: {i}\n')
-	        os.system(f"cp {i} /sdcard/Statuses/Videos")
-	        sleep(0.3)
+		print("Saving Videos")
+		videos = tqdm(glob('/storage/emulated/0/Android/media/com.whatsapp/Whatsapp/Media/.Statuses/*.mp4'))
+		for i in videos:
+			date_made = datetime.datetime.fromtimestamp(os.path.getmtime(i))
+			extension = os.path.splitext(i)[1]
+			new_name = f"{date_made.strftime("%Y-%m-%d %H:%M:%S")}{extension}"
+			new_file_path = os.path.join(VIDS, new_name)
+			with open(i, 'rb') as f:
+				video = f.read()
+			with open(new_file_path, "wb") as f:
+				f.write(video)
+			sleep(0.3)
 
 	elif choice.lower() == '3':
-	    print("Saving both Videos and Pictures")
-	    sleep(2)
-	    
-	    for i in pics:
-	        print(f'Copying: {i}\n')
-	        os.system(f"cp {i} /sdcard/Statuses/Pics")
-	    for i in videos:
-	        print(f'Copying: {i}\n')
-	        os.system(f"cp {i} /sdcard/Statuses/Videos")
+		print("Saving both Videos and Pictures")
+		videos = glob('/storage/emulated/0/Android/media/com.whatsapp/Whatsapp/Media/.Statuses/*.mp4')
+		pics = glob('/storage/emulated/0/Android/media/com.whatsapp/Whatsapp/Media/.Statuses/*.jpg')
+		all_ = tqdm(videos + pics)
+
+		sleep(2)
+		for i in all_:
+			date_made = datetime.datetime.fromtimestamp(os.path.getmtime(i))
+			extension = os.path.splitext(i)[1]
+			new_name = f"{date_made.strftime("%Y-%m-%d %H:%M:%S")}{extension}"
+			if extension == ".mp4":
+				new_file_path = os.path.join(VIDS, new_name)
+			elif extension == ".jpg":
+				new_file_path = os.path.join(PICS, new_name)
+			with open(i, 'rb') as f:
+				file = f.read()
+			with open(new_file_path, "wb") as f:
+				f.write(file)
+			sleep(0.3)
 
 	elif choice.lower() == '4':
 		print('Thank you for using Status Saver.')
@@ -73,3 +96,4 @@ while choice != '4':
 		copy_selection()
 	else:
 		print('Please select either 1 or 2')
+print('\nThank you for using Status Saver.')

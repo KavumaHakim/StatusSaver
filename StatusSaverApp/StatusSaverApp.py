db18bca130d364f0fa54c07b67f1a3ecef7cde3f
deidave <atudav15@gmail.com>
2025-02-03 18:49:26 +0300

    FIX: Removed duplicate images
    FEAT: Changed image card generation mechanism for better integration

--- StatusSaverApp/StatusSaverApp.py
+++ StatusSaverApp/StatusSaverApp.py
@@ -1,7 +1,6 @@
+from kivy.uix.image import Image
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window 
 from kivy.lang import Builder
@@ -10,7 +9,6 @@ from kivy.lang import Builder
 image_paths = []
 for i in range(10):
 	image_paths.append('sample_image.jpg')
-print(image_paths)
 
 
 class MyScreenManager(ScreenManager):
@@ -18,12 +16,16 @@ class MyScreenManager(ScreenManager):
 
 class HomeScreen(Screen):
 	def picture(self):
-		self.load_images(image_paths)
-		self.manager.current = 'images'
-	def load_images(self, image_paths):
-		for image in image_paths:
+		self.load_images()
+		self.manager.current = 'image_screen'
+
+	def load_images(self, image_path=image_paths):
+		image_screen = self.manager.get_screen('image_screen')  # Get ImageScreen
+		for image in image_path:
 			preview = ImageCard()
-			self.manager.ids['images'].ids['layout'].add_widget(preview)
+			img = Image(source=image, pos_hint={'center_x': .5, 'center_y': .5})
+			preview.add_widget(img)
+			image_screen.ids.layout.add_widget(preview)  # Use ImageScreen's layout
 
 
 class ImageScreen(Screen):
@@ -37,12 +39,12 @@ class StatusSaverApp(MDApp):
 		Builder.load_file('StatusSaver.kv')
 		Window.clearcolor = (1, 1, 1, 1)
 		my_manager = MyScreenManager()
-#		home_screen = HomeScreen(name = 'home') 
-#		image_screen = ImageScreen(name = 'images')
-#		my_manager.add_widget(home_screen)
-#		my_manager.add_widget(image_screen)
+		home_screen = HomeScreen(name = 'home')
+		image_screen = ImageScreen(name = 'image_screen')
+		my_manager.add_widget(home_screen)
+		my_manager.add_widget(image_screen)
 		return my_manager
-	
+
 
 
 if __name__ == '__main__':

45a9e4a7a143d9723c07a84307fbd2bc8bb209da
deidave <atudav15@gmail.com>
2025-02-06 13:08:04 +0300

    FIX: Tab selector customisations on image screen

--- StatusSaverApp/StatusSaverApp.py
+++ StatusSaverApp/StatusSaverApp.py
@@ -1,9 +1,8 @@
-from kivy.uix.screenmanager import FallOutTransition
-from kivy.uix.screenmanager import RiseInTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition, FallOutTransition
+from kivymd.uix.segmentedbutton import MDSegmentedButtonItem
+from kivymd.uix.card import MDCard
 from kivy.uix.image import Image
 from kivymd.app import MDApp
-from kivymd.uix.card import MDCard
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window 
 from kivy.lang import Builder
 from glob import glob
@@ -26,6 +25,7 @@ image_paths_saved = glob('/storage/emulated/0/Statuses/*.jpg')
 class MyScreenManager(ScreenManager):
 	pass
 
+
 class HomeScreen(Screen):
 	def picture(self):
 		# self.load_images()
@@ -38,7 +38,6 @@ class HomeScreen(Screen):
 	# 		img = Image(source=image, pos_hint={'center_x': .5, 'center_y': .5})
 	# 		preview.add_widget(img)
 	# 		image_screen.ids.layout.add_widget(preview)  # Use ImageScreen's layout
-
 
 class ImageScreen(Screen):
 	def expand(self, src):
@@ -66,8 +65,16 @@ class ImageViewer(Screen):
 		self.manager.transition = FallOutTransition()
 		self.manager.current = 'image_screen'
 
+
+class CustomSegment(MDSegmentedButtonItem):
+	def on_active(self, instance, value):
+		self.check_visible = False
+
+
 class ImageCard(MDCard):
 	pass
+
+
 
 class StatusSaverApp(MDApp):
 	def build(self):

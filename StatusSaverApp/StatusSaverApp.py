f33e1bf72eb4fa58a5aa660d5cb77a4f40dc9d13
deidave <atudav15@gmail.com>
2025-02-06 11:57:09 +0300

    FEAT: Switching tabs between saved and all pictures

--- StatusSaverApp/StatusSaverApp.py
+++ StatusSaverApp/StatusSaverApp.py
@@ -8,32 +8,58 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from glob import glob
 
+# '''Change this back before push'''
+# image_paths_all = []
+# image_paths_saved = []
+# for i in range(10):
+# 	image_paths_all.append('sample_image.jpg')
+# for i in range(10):
+# 	image_paths_saved.append('video-play-icon.png')
 
-image_paths = glob('/storage/emulated/0/Android/media/com.whatsapp/Whatsapp/Media/.Statuses/*.jpg')
+# -----READ ME------- #
+#Leave the above declractions in the code since I need them to test the UI in my computer
+#I only use the phone for small adjustments
+# --------END--------#
+image_paths_all = glob('/storage/emulated/0/Android/media/com.whatsapp/Whatsapp/Media/.Statuses/*.jpg')
+image_paths_saved = glob('/storage/emulated/0/Statuses/*.jpg')
 
 class MyScreenManager(ScreenManager):
 	pass
 
 class HomeScreen(Screen):
 	def picture(self):
-		self.load_images()
+		# self.load_images()
 		self.manager.current = 'image_screen'
 
-	def load_images(self, image_path=image_paths):
-		image_screen = self.manager.get_screen('image_screen')  # Get ImageScreen
-		for image in image_path:
-			preview = ImageCard()
-			img = Image(source=image, pos_hint={'center_x': .5, 'center_y': .5})
-			preview.add_widget(img)
-			image_screen.ids.layout.add_widget(preview)  # Use ImageScreen's layout
+	# def load_images(self, image_path=image_paths_all):
+	# 	image_screen = self.manager.get_screen('image_screen')  # Get ImageScreen
+	# 	for image in image_path:
+	# 		preview = ImageCard()
+	# 		img = Image(source=image, pos_hint={'center_x': .5, 'center_y': .5})
+	# 		preview.add_widget(img)
+	# 		image_screen.ids.layout.add_widget(preview)  # Use ImageScreen's layout
 
 
 class ImageScreen(Screen):
 	def expand(self, src):
 		image_view = self.manager.get_screen('image_view')
-		image_view.ids['view_img'].source = src
+		image_view.ids.view_img.source = src
 		self.manager.transition = RiseInTransition()
 		self.manager.current = 'image_view'
+	def change_content(self, tab):
+		content_grid = self.ids.layout
+		content_grid.clear_widgets()
+		if tab == 'all_tab':
+			image_path = image_paths_all
+		elif tab == 'saved_tab':
+			image_path = image_paths_saved
+		else:
+			return
+		for image in image_path:
+			preview = ImageCard()
+			img = Image(source=image, pos_hint={'center_x': .5, 'center_y': .5})
+			preview.add_widget(img)
+			content_grid.add_widget(preview)
 
 class ImageViewer(Screen):
 	def contract(self):

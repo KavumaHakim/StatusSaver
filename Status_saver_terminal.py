df2f2c66e9a2dc950997490b0049615ce82c5c99
Hakim <hakimkavuma99@gmail.com.com>
2025-02-16 14:01:51 +0300

    Add use of package status in terminal version. add use of commandline arguments - 1,2 and 3

--- Status_saver_terminal.py
+++ Status_saver_terminal.py
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/python
 from glob import glob
 from Status import Status
-from time import sleep
 import datetime
 from tqdm import tqdm
+import sys
 import os
 banner = '''
 ░██████╗ ████████╗ ░█████╗░ ████████╗ ██╗░░░██╗ ░██████╗
@@ -21,7 +21,10 @@ banner = '''
         ╚═════╝░ ╚═╝░░╚═╝ ░░░╚═╝░░░ ╚══════╝ ╚═╝░░╚═╝
         '''
 
-def main():
+def main(c: int = 0) -> None:
+    v = glob('/sdcard/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/*mp4')
+    p = glob('/sdcard/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/*jpg')
+
     os.system("clear")
     print(banner)
     print("Choose What Statuses to save:")
@@ -29,22 +32,27 @@ def main():
     print("2. Videos")
     print("3. Both")
     print("4. Exit")
-    global choice 
     n = 1
+    choice = c
     while n:
-        choice = input('Enter choice: ')
+        if choice == 0:
+            choice = input('Enter choice: ')
         if choice == '1':
             print('Saving Pics')
-            for i in tqdm(glob('/sdcard/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/*jpg')):
-                Status('pics', i)     
+            for i in tqdm(p, colour="yellow", desc="Pics"):
+                Status(file_type='pics', path=i)    
             n = 0
         elif choice == '2':
-            print('Saving videos')
-            Status().save_video()
+            print("Saving Videos")
+            for i in tqdm(v, colour="Red", desc="Vids"):
+                Status(file_type="video", path=i)
             n = 0
         elif choice == '3':
-            print('Saving all')
-            Status().save_both()
+            print("Saving Pics and Videos")
+            for i in tqdm(v, colour="Red", desc="Vids"):
+                Status(file_type="video", path=i)
+            for j in tqdm(p, colour="yellow", desc="Pics"):
+                Status(file_type="pics", path=j)
             n = 0
         elif choice == '4':
             exit('Thank you for using Status saver')
@@ -55,6 +63,9 @@ def main():
         print('Thank you for Using status saver')
         if input('Save sth else? y/n > ').lower() == 'y':
             main()
-
+            
 if __name__ == "__main__":
-    main()
+    try:
+        main(sys.argv[1])
+    except:
+        main()
